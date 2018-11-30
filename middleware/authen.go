483f731f74cd3085845021d677ed303887c3b7af
piouslove <305053045@qq.com>
2018-11-30 12:48:27 +0800

    errors handles

--- middleware/authen.go
+++ middleware/authen.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
 	"time"
-	"strings"
-	"errors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/idhubnetwork/jsontokens"
 )
+
+const DESTINATION = "url"
 
 type Token struct {
 	token string `form:"token" json:"token" xml:"token" binding:"required"`
@@ -17,26 +19,26 @@ func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tmp, err := searchToken(c)
 		if err != nil {
-			c.JSON(http.StatusForbidden, err)
+			c.JSON(http.StatusForbidden, err.Error())
 		}
 		jt := jsontokens.NewJsonToken()
 		jt.SetToken(tmp)
-		if err = jt.Verify(), err != nil {
-			c.JSON(http.StatusForbidden, err)
+		if err = jt.Verify(); err != nil {
+			c.JSON(http.StatusForbidden, err.Error())
 		}
 		expiration, ok := jt.Get("expiration").(int)
 		if !ok {
-			c.JSON(http.StatusForbidden, errors.New("jsontoken non expiration"))
+			c.JSON(http.StatusForbidden, "jsontoken non expiration")
 		}
 		if int64(expiration) > time.Now().Unix() {
-			c.JSON(http.StatusForbidden, errors.New("jsontoken expired"))
+			c.JSON(http.StatusForbidden, "jsontoken expired")
 		}
 		destination, ok := jt.Get("destination").(string)
 		if !ok {
-			c.JSON(http.StatusForbidden, errors.New("jsontoken non destination"))
+			c.JSON(http.StatusForbidden, "jsontoken non destination")
 		}
 		if destination != DESTINATION {
-			c.JSON(http.StatusForbidden, errors.New("invalid access url destination"))
+			c.JSON(http.StatusForbidden, "invalid access url destination")
 		}
 		c.Set("DIDJsonToken", jt)
 	}
@@ -45,12 +47,12 @@ func Authentication() gin.HandlerFunc {
 func searchToken(c *gin.Context) (string, error) {
 	tmp := c.Request.Header.Get("Authorization")
 	var token Token
-	if len(tmp) < 14 || tmp[0, 13] != "DIDJsonToken " {
-		if err := c.ShouldBind(&token), err != nil {
+	if len(tmp) < 14 || tmp[0:13] != "DIDJsonToken " {
+		if err := c.ShouldBind(&token); err != nil {
 			return "", errors.New("non DID Json Token")
 		}
-		tmp = token.token 
-		if len(tmp) < 14 || tmp[0, 13] != "DIDJsonToken " {
+		tmp = token.token
+		if len(tmp) < 14 || tmp[0:13] != "DIDJsonToken " {
 			return "", errors.New("invalid DID Json Token")
 		}
 		return "", errors.New("invalid DID Json Token")
