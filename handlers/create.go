6bb941b9d19493b10249a6f1c99ea558d1b708df
piouslove <305053045@qq.com>
2018-12-03 11:26:57 +0800

    create handler

--- handlers/create.go
+++ handlers/create.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/idhubnetwork/jsontokens"
 )
@@ -10,4 +12,29 @@ const (
 	DEFAULT_STATUS = 0x30
 )
 
-func CreateCredential(c *gin.Context, jt *jsontokens.JsonToken) {}
+func CreateCredential(c *gin.Context, jt *jsontokens.JsonToken) {
+	did, ok := jt.Get("did").(string)
+	if !ok || len(did) != 32 {
+		c.JSON(http.StatusForbidden, ActionErr{"jsontoken invalid or non did"})
+	}
+
+	var jwt JWT
+	err := c.ShouldBind(&jwt)
+	if err != nil {
+		c.JSON(http.StatusForbidden, ActionErr{"invalid or non jwt to create"})
+	}
+
+	tmp := jsontokens.NewJWT()
+	err = tmp.SetJWT(jwt.JsonWebToken)
+	if err != nil {
+		c.JSON(http.StatusForbidden, ActionErr{"invalid jwt to create"})
+	}
+	err = tmp.Verify()
+	if err != nil {
+		c.JSON(http.StatusForbidden, ActionErr{"invalid jwt signature"})
+	}
+
+	if did != tmp.Get("iss").(string) {
+		c.JSON(http.StatusForbidden, ActionErr{"non jwt issuer can not create"})
+	}
+}
