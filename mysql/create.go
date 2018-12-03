657623b376a4508044ed71d39ebbcf5b239cd609
piouslove <305053045@qq.com>
2018-12-03 15:55:21 +0800

    credential create

--- mysql/create.go
+++ mysql/create.go
@@ -1,1 +1,34 @@
 package db_mysql
+
+const (
+	// 0011 0000
+	DEFAULT_STATUS = 0x30
+)
+
+func CreateCredential(credential *Credential) error {
+	status := DEFAULT_STATUS | credential.Status
+
+	result, err := DB_mysql.Exec(`insert into credentials(iss,
+	sub,aud,exp,nbf,iat,jti,net,ipfs,context,credential,status)`,
+		credential.Iss,
+		credential.Sub,
+		credential.Aud,
+		credential.Exp,
+		credential.Nbf,
+		credential.Iat,
+		credential.Jti,
+		credential.Net,
+		credential.IPFS,
+		credential.Context,
+		credential.Credential,
+		status)
+
+	if err != nil {
+		return err
+	}
+	_, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	return nil
+}
