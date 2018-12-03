6af52d649d0ccf122ee3bf620027393e7bb15886
piouslove <305053045@qq.com>
2018-12-03 17:46:38 +0800

    delete credentials

--- mysql/delete.go
+++ mysql/delete.go
@@ -1,5 +1,30 @@
 package db_mysql
 
-func DeleteCredential(jwt_id int) {}
+func DeleteCredential(jwt_id int) error {
+	result, err := DB_mysql.Exec("delete from credentials where jwt_id = ?",
+		jwt_id)
 
-func DeleteCredential_TBD(jwt_id, status int) {}
+	if err != nil {
+		return err
+	}
+	_, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteCredential_TBD(jwt_id, status int) error {
+	result, err := DB_mysql.Exec("update credentials set status = ? where jwt_id = ?",
+		status,
+		jwt_id)
+
+	if err != nil {
+		return err
+	}
+	_, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	return nil
+}
