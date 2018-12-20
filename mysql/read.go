748daf10594336ae820c45872a37371a321a3df5
piouslove <305053045@qq.com>
2018-12-20 15:56:59 +0800

    fix read credential bug

--- mysql/read.go
+++ mysql/read.go
@@ -2,14 +2,15 @@ package db_mysql
 
 // Credential READ in mysql.
 // args is []string{iss, sub, aud, jti}
-func GetCredential(args ...string) (credential *Credential, err error) {
+func GetCredential(args ...string) (*Credential, error) {
+	credential := new(Credential)
 	row := DB_mysql.QueryRow("select credential from credentials where iss=? and sub=? and aud=? and jti=?",
 		args[0], args[1], args[2], args[3])
-	err = row.Scan(credential.Credential)
+	err := row.Scan(&credential.Credential)
 	if err != nil {
 		return nil, err
 	}
-	return
+	return credential, nil
 }
 
 // Credentials array READ in mysql.
@@ -21,9 +22,13 @@ func GetCredentials(args ...string) (credentials []*Credential, err error) {
 		return nil, err
 	}
 	defer rows.Close()
+	var tmp string
 	i := 0
 	for rows.Next() {
-		err = rows.Scan(credentials[i].Credential)
+		err = rows.Scan(&tmp)
+		credential := new(Credential)
+		credential.Credential = tmp
+		credentials = append(credentials, credential)
 		if err != nil {
 			return nil, err
 		}
